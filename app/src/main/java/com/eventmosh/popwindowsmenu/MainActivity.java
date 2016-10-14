db533333374030f87861b19109c6a2f2025d95cd
lixinxin <895330766@qq.com>
2016-10-14 17:33:30 +0800

    illlll

--- app/src/main/java/com/eventmosh/popwindowsmenu/MainActivity.java
+++ app/src/main/java/com/eventmosh/popwindowsmenu/MainActivity.java
@@ -1,9 +1,13 @@
 package com.eventmosh.popwindowsmenu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,10 +32,45 @@ public class MainActivity extends AppCompatActivity implements PopWindowsMenu.On
         mPopWindowsMenu.setMenuData(list);
         mPopWindowsMenu.setOnItemMenuClickListener(this);
         mPopWindowsMenu.setSelectPosition(3);
+
+
     }
 
     @Override
     public void onItemMenuClick(int position) {
         Toast.makeText(this, "lee" + position, Toast.LENGTH_SHORT).show();
     }
+
+
+    private void save() {
+        try {
+            FileOutputStream fos = openFileOutput("lxx.txt", Activity.MODE_PRIVATE);
+            fos.write("lixinin".getBytes());
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void read() {
+        try {
+            FileInputStream fis = openFileInput("lxx.txt");
+            byte[] bytes = new byte[1024];
+            ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+            while (fis.read(bytes) != -1) {
+                arrayOutputStream.write(bytes, 0, bytes.length);
+            }
+            fis.close();
+            arrayOutputStream.close();
+            String content = new String(arrayOutputStream.toByteArray());
+            content=content.trim();
+            Toast.makeText(this, content, Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
